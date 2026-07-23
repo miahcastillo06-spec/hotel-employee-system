f3f38a3397b7d8085002f6ba73d28db7039cb423
miah <miahcastillo06@gmail.com>
2026-07-23 23:03:37 +0800

    Update and rename config.js to hotel_system/config.js

--- hotel_system/config.js
+++ hotel_system/config.js
@@ -5,4 +5,4 @@
 // physical PC (PC2), replace "localhost" with PC2's IP address,
 // e.g. "http://192.168.1.20/employee_system/api_get_employees.php"
 // ==========================================================
-const EMPLOYEE_API_URL = "http://localhost/hotel-employee-system/employee_system/api_get_employees.php";
+const EMPLOYEE_API_URL = "http://localhost/hotel-employee-system/employee_system/get_employees.php";
