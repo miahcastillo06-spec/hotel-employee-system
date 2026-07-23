00a351ad3ed714443980bec2c9b6831dc986f4d4
miah <miahcastillo06@gmail.com>
2026-07-23 23:13:53 +0800

    Update hotel_system.sql

--- database/hotel_system.sql
+++ database/hotel_system.sql
@@ -2,14 +2,6 @@
 --  HOTEL SYSTEM DATABASE
 --  Run this in phpMyAdmin / MySQL CLI on PC1 / Web Server 1
 -- ==========================================================
--- Entity: reservation
--- Attributes match the diagram:
---   1. name customer
---   2. check in
---   3. check out
---   4. employee name
---   5. employee id
--- ==========================================================
 
 CREATE DATABASE IF NOT EXISTS hotel_db;
 USE hotel_db;
